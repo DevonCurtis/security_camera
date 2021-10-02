b2b2cd48044d92ecf4e57615eb9f21ea817fd344
DevonCurtis <devoncurtis8@gmail.com>
2021-10-02 18:51:19 -0400

    created two frame to track the difference of two positions

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import cv2 #imported Opencv library
 cam = cv2.VideoCapture(0)
 # while camera open, read camera
 while cam.isOpened():
-    ret, frame = cam.read()
+    #Capturing two instances
+    ret, frame1 = cam.read()
+    ret, frame2 = cam.read()
+    diff = cv2.absdiff(frame1, frame2)
     # if the 'A' key is pressed wait 10 milliseconds then destroy window
     if cv2.waitKey(10) == ord('a'):
         break
-    cv2.imshow('Security cam', frame)
+    cv2.imshow('Security cam', diff)
